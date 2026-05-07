998687b8bc98f9230d80ae3ff7b570d35080962f
Walt Wu <wus9@legal.regn.net>
2026-05-07 14:15:06 +0800

    Fix context compaction exports

--- crates/pith-core/src/context/context_compaction.rs
+++ crates/pith-core/src/context/context_compaction.rs
@@ -1,6 +1,5 @@
 pub use super::context_memory_pack::{
-  format_context_prompt, merge_context_pack_attributes, pack_memory_context,
-  pack_relevant_memory_notes, ContextPack, CONTEXT_MEMORY_NOTE_LIMIT,
+  format_context_prompt, merge_context_pack_attributes, pack_memory_context, ContextPack,
 };
 pub use super::context_observation::{
   compact_prompt_observation, merge_observation_attributes, PromptObservation,
