9afc95c79c4d45ef39397029d0fb80619dcd27bb
Walt Wu <wus9@legal.regn.net>
2026-05-09 15:01:42 +0800

    Separate plugin view model presentation

---
+++ apps/pith-macos/Sources/PithApp/Plugins/AppViewModel+PluginPresentation.swift
@@ -0,0 +1,92 @@
+import Foundation
+
+@MainActor
+extension AppViewModel {
+  func pluginCountSummary() -> String {
+    PluginDashboardPresenter.pluginCountSummary(pluginDashboardSnapshot)
+  }
+
+  func localPluginCountSummary() -> String {
+    PluginDashboardPresenter.localPluginCountSummary(pluginDashboardSnapshot)
+  }
+
+  func pluginDetailSummary() -> String {
+    PluginDashboardPresenter.pluginDetailSummary(pluginDashboardSnapshot)
+  }
+
+  func pluginCatalogPreview() -> [PluginSummary] {
+    PluginDashboardPresenter.catalogPreview(pluginDashboardSnapshot)
+  }
+
+  func pluginPermissionCountSummary() -> String {
+    PluginDashboardPresenter.permissionCountSummary(pluginDashboardSnapshot)
+  }
+
+  func pluginPermissionDetailSummary() -> String {
+    PluginDashboardPresenter.permissionDetailSummary(pluginDashboardSnapshot)
+  }
+
+  func pluginPermissionPreview() -> [PluginSummary] {
+    PluginDashboardPresenter.permissionPreview(pluginDashboardSnapshot)
+  }
+
+  func invalidPluginCountSummary() -> String {
+    PluginDashboardPresenter.invalidPluginCountSummary(pluginDashboardSnapshot)
+  }
+
+  func invalidPluginDetailSummary() -> String {
+    PluginDashboardPresenter.invalidPluginDetailSummary(pluginDashboardSnapshot)
+  }
+
+  func invalidPlugins() -> [PluginSummary] {
+    PluginDashboardPresenter.invalidPlugins(pluginDashboardSnapshot)
+  }
+
+  func pluginRegistryCountSummary() -> String {
+    PluginDashboardPresenter.registryCountSummary(pluginDashboardSnapshot)
+  }
+
+  func pluginRegistryDetailSummary() -> String {
+    PluginDashboardPresenter.registryDetailSummary(pluginDashboardSnapshot)
+  }
+
+  func pluginCapabilityPreview() -> [PluginCapabilitySummary] {
+    PluginDashboardPresenter.capabilityPreview(pluginDashboardSnapshot)
+  }
+
+  func pluginConnectorCountSummary() -> String {
+    PluginDashboardPresenter.connectorCountSummary(pluginDashboardSnapshot)
+  }
+
+  func pluginConnectorDetailSummary() -> String {
+    PluginDashboardPresenter.connectorDetailSummary(pluginDashboardSnapshot)
+  }
+
+  func pluginConnectorPreview() -> [PluginConnectorSummary] {
+    PluginDashboardPresenter.connectorPreview(pluginDashboardSnapshot)
+  }
+
+  func pluginCommandCountSummary() -> String {
+    PluginDashboardPresenter.commandCountSummary(pluginDashboardSnapshot)
+  }
+
+  func pluginCommandDetailSummary() -> String {
+    PluginDashboardPresenter.commandDetailSummary(pluginDashboardSnapshot)
+  }
+
+  func pluginCommandPreview() -> [PluginCommandSummary] {
+    PluginDashboardPresenter.commandPreview(pluginDashboardSnapshot)
+  }
+
+  func pluginHookCountSummary() -> String {
+    PluginDashboardPresenter.hookCountSummary(pluginDashboardSnapshot)
+  }
+
+  func pluginHookDetailSummary() -> String {
+    PluginDashboardPresenter.hookDetailSummary(pluginDashboardSnapshot)
+  }
+
+  func pluginHookPreview() -> [PluginHookSummary] {
+    PluginDashboardPresenter.hookPreview(pluginDashboardSnapshot)
+  }
+}
--- apps/pith-macos/Sources/PithApp/Plugins/AppViewModel+Plugins.swift
+++ apps/pith-macos/Sources/PithApp/Plugins/AppViewModel+Plugins.swift
@@ -152,46 +152,6 @@ extension AppViewModel {
     PluginActionPlanner.canRunCommand(commandID: commandID, snapshot: pluginActionSnapshot())
   }
 
-  func pluginCountSummary() -> String {
-    PluginDashboardPresenter.pluginCountSummary(pluginDashboardSnapshot)
-  }
-
-  func localPluginCountSummary() -> String {
-    PluginDashboardPresenter.localPluginCountSummary(pluginDashboardSnapshot)
-  }
-
-  func pluginDetailSummary() -> String {
-    PluginDashboardPresenter.pluginDetailSummary(pluginDashboardSnapshot)
-  }
-
-  func pluginCatalogPreview() -> [PluginSummary] {
-    PluginDashboardPresenter.catalogPreview(pluginDashboardSnapshot)
-  }
-
-  func pluginPermissionCountSummary() -> String {
-    PluginDashboardPresenter.permissionCountSummary(pluginDashboardSnapshot)
-  }
-
-  func pluginPermissionDetailSummary() -> String {
-    PluginDashboardPresenter.permissionDetailSummary(pluginDashboardSnapshot)
-  }
-
-  func pluginPermissionPreview() -> [PluginSummary] {
-    PluginDashboardPresenter.permissionPreview(pluginDashboardSnapshot)
-  }
-
-  func invalidPluginCountSummary() -> String {
-    PluginDashboardPresenter.invalidPluginCountSummary(pluginDashboardSnapshot)
-  }
-
-  func invalidPluginDetailSummary() -> String {
-    PluginDashboardPresenter.invalidPluginDetailSummary(pluginDashboardSnapshot)
-  }
-
-  func invalidPlugins() -> [PluginSummary] {
-    PluginDashboardPresenter.invalidPlugins(pluginDashboardSnapshot)
-  }
-
   func isRemovablePlugin(_ plugin: PluginSummary) -> Bool {
     PluginActionPlanner.isRemovable(plugin)
   }
@@ -214,54 +174,6 @@ extension AppViewModel {
       plugin.manifestPath,
       successDetail: "Revealed \(plugin.displayName) manifest."
     )
-  }
-
-  func pluginRegistryCountSummary() -> String {
-    PluginDashboardPresenter.registryCountSummary(pluginDashboardSnapshot)
-  }
-
-  func pluginRegistryDetailSummary() -> String {
-    PluginDashboardPresenter.registryDetailSummary(pluginDashboardSnapshot)
-  }
-
-  func pluginCapabilityPreview() -> [PluginCapabilitySummary] {
-    PluginDashboardPresenter.capabilityPreview(pluginDashboardSnapshot)
-  }
-
-  func pluginConnectorCountSummary() -> String {
-    PluginDashboardPresenter.connectorCountSummary(pluginDashboardSnapshot)
-  }
-
-  func pluginConnectorDetailSummary() -> String {
-    PluginDashboardPresenter.connectorDetailSummary(pluginDashboardSnapshot)
-  }
-
-  func pluginConnectorPreview() -> [PluginConnectorSummary] {
-    PluginDashboardPresenter.connectorPreview(pluginDashboardSnapshot)
-  }
-
-  func pluginCommandCountSummary() -> String {
-    PluginDashboardPresenter.commandCountSummary(pluginDashboardSnapshot)
-  }
-
-  func pluginCommandDetailSummary() -> String {
-    PluginDashboardPresenter.commandDetailSummary(pluginDashboardSnapshot)
-  }
-
-  func pluginCommandPreview() -> [PluginCommandSummary] {
-    PluginDashboardPresenter.commandPreview(pluginDashboardSnapshot)
-  }
-
-  func pluginHookCountSummary() -> String {
-    PluginDashboardPresenter.hookCountSummary(pluginDashboardSnapshot)
-  }
-
-  func pluginHookDetailSummary() -> String {
-    PluginDashboardPresenter.hookDetailSummary(pluginDashboardSnapshot)
-  }
-
-  func pluginHookPreview() -> [PluginHookSummary] {
-    PluginDashboardPresenter.hookPreview(pluginDashboardSnapshot)
   }
 
   func refreshPluginState() async {
